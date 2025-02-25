c77e844c831e6411298f2bdcaecfc4775804a9b6
crmSaaS <crmsaas.catination.com>
2025-02-25 16:08:06 +0530

    next.config chages done

--- next.config.js
+++ next.config.js
@@ -1,12 +1,25 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
   images: {
     remotePatterns: [
       {
         protocol: "https",
-        hostname: "**",
+        hostname: "**", // Allow all domains (not recommended for security)
       },
+    ],
+    domains: [
+      "www.dlf.in",
+      "www.omaxe.com",
+      "www.mahagunindia.com",
+      "cdn.in.emaar.com",
+      "www.ansalapi.com",
+      "www.eldecogroup.com",
+      "raheja.com",
+      "www.ajnara.co",
+      "infratech.thewavegroup.com",
+      "ireoprojects.co.in",
+      "www.unitechgroup.com",
+      "www.gaursonsindia.com",
     ],
   },
 };
